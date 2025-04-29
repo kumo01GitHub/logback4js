c9cf4ffad0743cfda971b22480c6548c25a771d5
TAICHI <kumo01@outlook.com>
2025-04-29 11:33:54 +0900

    feat: introduce gulp (#2)

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  files: [
+    "src/**/*.ts",
+  ],
+  ignores: [
+    "**/*.spec.ts",
+  ],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+  ]
+});
