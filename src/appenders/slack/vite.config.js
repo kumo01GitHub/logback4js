1ce5105a78123c9f9483b0b6e5b48a52cba169d0
TAICHI <kumo01@outlook.com>
2025-05-11 23:14:44 +0900

    feat: support vite (#6)
    * feat: support vite
    
    * fix: tsconfig

---
+++ src/appenders/slack/vite.config.js
@@ -0,0 +1,30 @@
+import * as path from "node:path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import dts from "vite-plugin-dts";
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  return {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: path.resolve(__dirname, "package.json"),
+            dest: path.resolve(__dirname, "../../../dist/appenders/slack/"),
+          },
+        ],
+      }),
+      dts({ tsconfigPath: path.resolve(__dirname, "tsconfig.slack.json") }),
+      ,
+    ],
+    build: {
+      outDir: path.resolve(__dirname, "../../../dist/appenders/slack/"),
+      lib: {
+        name: "SlackAppenders",
+        entry: path.resolve(__dirname, "index.ts"),
+        formats: ['es', 'cjs', 'umd'],
+        fileName: (format, entryName) => `${entryName}.${format}.js`,
+      },
+    },
+  };
+});
---
+++ src/appenders/twitter/vite.config.js
@@ -0,0 +1,30 @@
+import * as path from "node:path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import dts from "vite-plugin-dts";
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  return {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: path.resolve(__dirname, "package.json"),
+            dest: path.resolve(__dirname, "../../../dist/appenders/twitter/"),
+          },
+        ],
+      }),
+      dts({ tsconfigPath: path.resolve(__dirname, "tsconfig.twitter.json") }),
+      ,
+    ],
+    build: {
+      outDir: path.resolve(__dirname, "../../../dist/appenders/twitter/"),
+      lib: {
+        name: "TwitterAppenders",
+        entry: path.resolve(__dirname, "index.ts"),
+        formats: ['es', 'cjs', 'umd'],
+        fileName: (format, entryName) => `${entryName}.${format}.js`,
+      },
+    },
+  };
+});
