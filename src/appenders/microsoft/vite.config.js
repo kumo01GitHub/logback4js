123ef2033fd3dfabaa0114f6b2ddd3db6eec02b2
kumo01GitHub <kumo01@outlook.com>
2025-05-11 23:13:03 +0900

    feat: support vite

---
+++ src/appenders/microsoft/vite.config.js
@@ -0,0 +1,30 @@
+import * as path from "node:path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import dts from "vite-plugin-dts";
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  return {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: path.resolve(__dirname, "package.json"),
+            dest: path.resolve(__dirname, "../../../dist/appenders/microsoft/"),
+          },
+        ],
+      }),
+      dts({ tsconfigPath: path.resolve(__dirname, "tsconfig.microsoft.json") }),
+      ,
+    ],
+    build: {
+      outDir: path.resolve(__dirname, "../../../dist/appenders/microsoft/"),
+      lib: {
+        name: "MicrosoftAppenders",
+        entry: path.resolve(__dirname, "index.ts"),
+        formats: ['es', 'cjs', 'umd'],
+        fileName: (format, entryName) => `${entryName}.${format}.js`,
+      },
+    },
+  };
+});
