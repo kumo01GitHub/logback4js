0784433fd6cae2102bde67e19cbaa7b3f6cd0b66
kumo01GitHub <kumo01@outlook.com>
2025-06-14 15:40:32 +0900

    feat(database): support redis

--- src/appenders/database/index.ts
+++ src/appenders/database/index.ts
@@ -7,4 +7,4 @@ export * from './database.appender';
 export * from './mysql.appender';
 export * from './postgres.appender';
 export * from './sqlite.appender';
-// export * from './redis.appender';
+export * from './redis.appender';
