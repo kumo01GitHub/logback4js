b3803876cd361f36c94c9fd821f086676a684b8f
kumo01GitHub <kumo01@outlook.com>
2025-06-29 16:13:03 +0900

    ci(database): drop mysql from compose.yaml

--- mock/mysql/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE logback4js;
-GRANT ALL PRIVILEGES ON logback4js.* TO mysql@'%';
-USE logback4js;
-CREATE TABLE log (
-    logger text,
-    timestamp datetime,
-    level varchar(8),
-    message text
-);
