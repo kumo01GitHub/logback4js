9b7a71fa63f8e0a5bdb86a2213cc5a7a77eea3d2
kumo01GitHub <kumo01@outlook.com>
2025-07-11 00:00:31 +0900

    ci: improve coverage

--- src/core/appender/json.appender.spec.ts
+++ src/core/appender/json.appender.spec.ts
@@ -4,6 +4,9 @@ import { JsonAppender } from "./json.appender";
 
 describe("JsonAppender", () => {
   class SpecAppender extends JsonAppender {
+    constructor(template?: { [key: string]: any }) {
+      super(template);
+    }
     get name(): string {
       return "spec";
     }
@@ -41,6 +44,30 @@ describe("JsonAppender", () => {
       timestamp: event.timestamp,
       level: event.level.label,
       message: event.message,
+    });
+  });
+
+  it("can customize message", () => {
+    const timestamp: Date = new Date();
+    const id: number = 999;
+    const customMsgAppender: JsonAppender = new SpecAppender({
+      msg: "${level} : ${message}",
+      from: "${ appender } of ${ logger }",
+      datetime: "${ timestamp }",
+      id: id,
+    });
+    const event = {
+      logger: "SpecLogger",
+      timestamp: timestamp,
+      level: LogLevel.Info,
+      message: "Specification Test",
+    };
+
+    expect(customMsgAppender.getMessage(event)).toEqual({
+      msg: `${event.level.label} : ${event.message}`,
+      from: `${ customMsgAppender.name } of ${ event.logger }`,
+      datetime: timestamp.toISOString(),
+      id: id,
     });
   });
 });
